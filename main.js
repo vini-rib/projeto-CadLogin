7b0a3e542d2e6862231480d5dab16403d63e976d
vini-rib <viniciuscerconi2003@gmail.com>
2025-04-15 20:40:38 -0300

    adicionando função que valida se preencheu os campos, e redireciona para página de cadastro

---
+++ main.js
@@ -0,0 +1,14 @@
+// Implementa acesso para tela de login
+function acessar(){
+    let LoginEmail = document.
+    getElementById('LoginEmail').value;
+    let LoginSenha = document.
+    getElementById('LoginSenha').value;
+
+    //Implementa estrutura que testa se todos os campos foram preenchios
+    if(!LoginEmail || !LoginSenha){
+        alert('Favor preencher todos os campos');
+    }else{
+        window.location.href = "cadastro.html";
+    }
+}
